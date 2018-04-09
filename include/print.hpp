ce8f77a4631bf01d351f8d8a559e57affedd9a3c
MorozzoFFF <vaskanewstargood1999@mail.ru>
2018-04-09 22:15:33 +0000

    added sources

---
+++ include/print.hpp
@@ -0,0 +1,6 @@
+#include <string>
+#include <fstream>
+#include <iostream>
+
+void print(const std::string& text, std::ostream& out = std::cout);
+void print(const std::string& text, std::ofstream& out);
